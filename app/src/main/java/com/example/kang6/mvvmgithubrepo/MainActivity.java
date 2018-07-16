b98147a90e5958ed2e63e2f7add3136aa2b63979
kang <myeongwon.kang@vitalhint.com>
2018-07-16 19:02:18 +0900

    - 간단한 데이터 바인딩 추가

--- app/src/main/java/com/example/kang6/mvvmgithubrepo/MainActivity.java
+++ app/src/main/java/com/example/kang6/mvvmgithubrepo/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.kang6.mvvmgithubrepo;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+
+import com.example.kang6.mvvmgithubrepo.databinding.ActivityMainBinding;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.setUser(new User("kang", 37));
+
+        String date = (String) DateFormat.format("yyyy/MM/dd kk:mm:ss", Calendar.getInstance());
+        binding.textTime.setText(date);
     }
 }
